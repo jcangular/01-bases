f41ba9eb6e34d3cb64815e664581f03761e7c428
José Carlos Palma <palmahn@gmail.com>
2021-02-04 19:43:43 -0600

    Creación de página principal del módulo de dbz.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HerosModule } from './heros/heros.module';
 import { CountersModule } from './counters/counters.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         CountersModule,
-        HerosModule
+        HerosModule,
+        DbzModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
